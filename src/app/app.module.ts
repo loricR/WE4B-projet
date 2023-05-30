9cc23f60769500a7d8cbcf22d5ce9c78b212aaac
Iron68A <84898382+Iron68A@users.noreply.github.com>
2023-05-30 16:01:12 +0200

    Ajout Homecomponenent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AppComponent } from './app.component';
 import { GameComponent } from './views/game/game.component';
 import { GameListComponent } from './views/game-list/game-list.component';
 import { GamefullComponent } from './views/gamefull/gamefull.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
     GameListComponent,
-    GamefullComponent
+    GamefullComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
