29a1b64f14236713cc722fc028d73ada82a183e2
Juloroti <louis.rolland2002@gmail.com>
2023-06-01 18:29:41 +0200

    Début séparation backend/frontend -> lien élémentaire avec bdd, improvement soon

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { GameListComponent } from './views/game-list/game-list.component';
+import { GamefullComponent } from './views/gamefull/gamefull.component';
+import { HomeComponent } from './views/home/home.component';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+
+const routes: Routes = [
+
+  {path:'create', component:CreateComponent},
+  {path:'read', component:ReadComponent},
+  {path: '', component: HomeComponent},
+  {path: 'store', component: GameListComponent},
+  {path: 'game/:id', component : GamefullComponent}
+   ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GameComponent } from './views/game/game.component';
+import { GameListComponent } from './views/game-list/game-list.component';
+import { GamefullComponent } from './views/gamefull/gamefull.component';
+import { NavbarComponent } from './views/navbar/navbar.component';
+import { HomeComponent } from './views/home/home.component';
+import { CreateComponent } from './create/create.component';
+import { ReadComponent } from './read/read.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import { ApiserviceService } from './apiservice.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    GameComponent,
+    GameListComponent,
+    GamefullComponent,
+    NavbarComponent,
+    HomeComponent,
+    CreateComponent,
+    ReadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    YouTubePlayerModule,
+    HttpClientModule
+  ],
+  providers: [ApiserviceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
