b821c98208cf6ef37bd5f6536f88d8c180fcec3e
tmayer54 <thibault.mayer54@gmail.com>
2023-06-06 14:48:08 +0200

    ajout barre de recherche

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,10 +14,16 @@ import { ReadComponent } from './read/read.component';
 
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './views/signup/signup.component';
+<<<<<<< HEAD:frontend/src/app/app.module.ts
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ApiserviceService } from './apiservice.service';
 import { HttpClientModule } from '@angular/common/http';
 
+=======
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { ResearchBarComponent } from './views/research-bar/research-bar.component';
+import { ResearchPageComponent } from './views/research-page/research-page.component';
+>>>>>>> 04e438f1 (debat barre de recherche):src/app/app.module.ts
 @NgModule({
   declarations: [
     AppComponent,
@@ -26,9 +32,15 @@ import { HttpClientModule } from '@angular/common/http';
     GamefullComponent,
     NavbarComponent,
     HomeComponent,
+<<<<<<< HEAD:frontend/src/app/app.module.ts
     CreateComponent,
     ReadComponent,
     SignupComponent
+=======
+    SignupComponent,
+    ResearchBarComponent,
+    ResearchPageComponent,
+>>>>>>> 04e438f1 (debat barre de recherche):src/app/app.module.ts
   ],
 imports: [
     CommonModule,
