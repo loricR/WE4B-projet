10aed3b4240fb53cec156f424315637fd1570ce2
Iron68A <84898382+Iron68A@users.noreply.github.com>
2023-06-11 15:26:10 +0200

    Modif route pour dev

--- frontend/src/app/views/game/game.component.ts
+++ frontend/src/app/views/game/game.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { Game } from 'src/app/models/game';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-game',
@@ -11,11 +12,15 @@ export class GameComponent implements OnInit {
   @Input() game!: Game;
   @Input() game_idx! : number;
 
-  constructor(private router: Router) {};
+  constructor(private router: Router,private GameService:GameService) {};
 
   ngOnInit(): void {};
 
   readMore() {
     this.router.navigate(['/', 'game', this.game_idx+1])
   }
+  madeby() : void {
+    this.router.navigate(['/', 'developer', this.game.dev])   // acceder au dev du jeu
+  }
+
 }
