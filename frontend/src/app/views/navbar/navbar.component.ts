07dca154c5a0220120f09cc9eb954beccf907a55
LORIC\loric <loric.ravassard@gmail.com>
2023-06-13 17:36:44 +0200

    Affichage de l'élément actif de la navbar correctement

--- frontend/src/app/views/navbar/navbar.component.ts
+++ frontend/src/app/views/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Event, NavigationEnd, Router } from '@angular/router';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
@@ -8,8 +9,45 @@ import { TokenStorageService } from 'src/app/services/token-storage.service';
 })
 export class NavbarComponent implements OnInit {
   isLoggedIn = false;
+  navItem: Map<string, boolean>;
 
-  constructor(private tokenStorage: TokenStorageService) { }
+  constructor(private tokenStorage: TokenStorageService, private router: Router) { 
+    this.navItem = new Map<string, boolean>();
+    this.navItem.set('home', false);
+    this.navItem.set('store', false);
+    this.navItem.set('actu', false);
+    this.navItem.set('research', false);
+    this.navItem.set('registrer', false);
+    router.events.subscribe((event: Event) => {
+      var currentUrl: string;
+      if(event instanceof NavigationEnd) {
+        currentUrl = event.url.split('/')[1]; //Get the url between / and / '/game/1' returns 'game'
+        switch(currentUrl) {
+          case '':
+            this.setActive('home');
+            break;
+          case 'store':
+          case 'game':
+          case 'developer':
+            this.setActive('store');
+            break;
+          case 'actu':
+            this.setActive('actu');
+            break;
+          case 'research':
+            this.setActive('research');
+            break;
+          case 'register':
+            this.setActive('register');
+            break;
+          default:
+            this.setActive('home');
+            break;
+        }
+
+      }
+    })
+  }
 
   ngOnInit(): void {
     if(this.tokenStorage.getToken()) {
@@ -20,5 +58,12 @@ export class NavbarComponent implements OnInit {
   logout(): void {
     this.tokenStorage.signOut();
     window.location.reload();
+  }
+
+  setActive(newActive: string): void {
+    [...this.navItem.keys()].forEach((key) => {
+      this.navItem.set(key, false);
+    })
+    this.navItem.set(newActive, true);
   }
 }
